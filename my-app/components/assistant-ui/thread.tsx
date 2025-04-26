4c70f8db627bce939b8a660167cf068be3ab8c8f
ramesh200110 <143710115+ramesh200110@users.noreply.github.com>
2025-04-26 12:41:26 +0530

    Update thread.tsx

--- my-app/components/assistant-ui/thread.tsx
+++ my-app/components/assistant-ui/thread.tsx
@@ -73,7 +73,7 @@ const ThreadWelcome: FC = () => {
     <ThreadPrimitive.Empty>
       <div className="flex w-full max-w-[var(--thread-max-width)] flex-grow flex-col">
         <div className="flex w-full flex-grow flex-col items-center justify-center">
-          <p className="mt-4 font-medium">How can I help you today?</p>
+          <p className="mt-4 font-medium">How can I help you today in Searching? </p>
         </div>
         <ThreadWelcomeSuggestions />
       </div>
